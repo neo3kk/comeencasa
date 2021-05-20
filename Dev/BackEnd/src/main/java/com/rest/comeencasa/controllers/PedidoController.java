1de74a3b07b1722a9371ad24e2a1f7848eca75eb
krimo <krimo.bensaid@gmail.com>
2021-05-20 15:54:21 +0200

    Creacion de controller para poder añadir un nuevo pedido

--- Dev/BackEnd/src/main/java/com/rest/comeencasa/controllers/PedidoController.java
+++ Dev/BackEnd/src/main/java/com/rest/comeencasa/controllers/PedidoController.java
@@ -5,13 +5,11 @@ import com.rest.comeencasa.entities.Pedido;
 import com.rest.comeencasa.entities.PedidoDTO;
 import com.rest.comeencasa.entities.Usuario;
 import com.rest.comeencasa.service.*;
+import com.rest.comeencasa.utils.utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -35,16 +33,17 @@ public class PedidoController {
     LoginServiceOauth loginServiceOauth;
 
 
-    @PostMapping ("/pedidos")
+    @PostMapping("/pedidos")
     public ResponseEntity<String> getAllByUser(@RequestHeader("Authorization") String auth) throws Exception {
         Usuario user = null;
         if (auth != null && !auth.isEmpty()) {
             String token = auth.replace("Bearer ", "");
             String validate = tokenService.verifyToken(token);
             Map<String, String> userDetails = loginServiceOauth.getUserDetails(token);
-            if(userDetails.get("email") != null){
+            if (userDetails.get("email") != null) {
                 validate = userDetails.get("email");
-            };
+            }
+            ;
             if (validate != null) {
                 user = userService.getUserByEmail(validate);
             }
@@ -53,6 +52,33 @@ public class PedidoController {
             List<Pedido> pedidos = pedidoService.findByUsuario(user);
             List<PedidoDTO> pedidoDTOList = pedidoService.createListpedidoDTO(pedidos);
             return new ResponseEntity<>(gson.toJson(pedidoDTOList), HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @PutMapping("/pedidos")
+    public ResponseEntity<String> createPedido(@RequestHeader("Authorization") String auth, @RequestBody String payload) throws Exception {
+        Usuario user = null;
+        if (auth != null && !auth.isEmpty()) {
+            String token = auth.replace("Bearer ", "");
+            String validate = tokenService.verifyToken(token);
+            Map<String, String> userDetails = loginServiceOauth.getUserDetails(token);
+            if (userDetails.get("email") != null) {
+                validate = userDetails.get("email");
+            }
+            if (validate != null) {
+                user = userService.getUserByEmail(validate);
+                Map<String, String> map = gson.fromJson(payload, HashMap.class);
+                String ubicacion = map.get("ubicacion");
+                String precio_final = map.get("precio_final");
+                Pedido pedido = new Pedido();
+                pedido.setFecha_pedido(utils.getToday());
+                pedido.setUbicacion(ubicacion);
+                pedido.setUser(user);
+                pedido.setPrecio_final(precio_final);
+                pedidoService.makePedidoDto(pedido);
+                return new ResponseEntity<>("Se ha realizado el pedido correctamente",HttpStatus.ACCEPTED);
+            }
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
