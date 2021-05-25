38666a29cf8edcdc1ef1448ff3b2906d75d04ae1
Jlopez <neo3kk@gmail.com>
2021-05-26 00:29:57 +0200

    pagina de admin, con enlace a las paginas de creacion de elementos ingrediente i plato

--- Dev/FrontEnd/src/pages/Admin.vue
+++ Dev/FrontEnd/src/pages/Admin.vue
@@ -1,8 +1,29 @@
 <template>
   <q-page>
-    {{admin}}
-    <div>
+    <div class="q-pa-md q-gutter-md fit column wrap justify-center items-center content-center">
+      <h3>Adminsitra ingredientes y platos</h3>
+      <q-card class="my-card">
+        <q-card-section class="bg-primary text-white">
+          <div class="text-h6">Ingredientes</div>
+          <div class="text-subtitle2">Edita la lista de ingredientes</div>
+        </q-card-section>
 
+        <q-separator />
+
+        <q-card-actions align="around">
+          <q-btn flat @click="$router.push('/admin/nuevoingrediente')">Edita</q-btn>
+        </q-card-actions>
+      </q-card>
+      <q-card class="my-card">
+        <q-card-section class="bg-purple text-white">
+          <div class="text-h6">Platos</div>
+          <div class="text-subtitle2">Crea los diferentes platos</div>
+        </q-card-section>
+
+        <q-card-actions align="around">
+          <q-btn flat @click="$router.push('/admin/nuevoplato')">Edita</q-btn>
+        </q-card-actions>
+      </q-card>
     </div>
   </q-page>
 
@@ -19,12 +40,12 @@ export default {
 
   data() {
     return {
-      admin:"",
+      admin: "",
       post: "",
       url_server_api: SETTINGS.URL_SERVER_API
     };
   },
-  created(){
+  created() {
     this.getPost()
   },
   methods: {
@@ -35,5 +56,11 @@ export default {
   }
 }
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
 
 
