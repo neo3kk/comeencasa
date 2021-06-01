31161ad8e2e0714d43ddce835c3792e90a667622
Jlopez <neo3kk@gmail.com>
2021-06-01 21:13:52 +0200

    traduccion del ingrediente se muestra por pantalla

--- Dev/BackEnd/src/main/java/com/rest/comeencasa/entities/Ingrediente.java
+++ Dev/BackEnd/src/main/java/com/rest/comeencasa/entities/Ingrediente.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.OnDeleteAction;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "ingrediente")
@@ -18,6 +19,9 @@ public class Ingrediente implements Serializable {
 
     @Column(name= "nombre")
     private String name;
+
+    @Column(name= "traduccion")
+    private String traduccion;
 
 
 
@@ -41,11 +45,42 @@ public class Ingrediente implements Serializable {
         this.name = name;
     }
 
-    public List<PlatoIngrediente> getPlatoIngrediente() {
+    public String getTraduccion() {
+        return traduccion;
+    }
+
+    public void setTraduccion(String traduccion) {
+        this.traduccion = traduccion;
+    }
+
+    public List<com.rest.comeencasa.entities.PlatoIngrediente> getPlatoIngrediente() {
         return PlatoIngrediente;
     }
 
-    public void setPlatoIngrediente(List<PlatoIngrediente> platoIngrediente) {
+    public void setPlatoIngrediente(List<com.rest.comeencasa.entities.PlatoIngrediente> platoIngrediente) {
         PlatoIngrediente = platoIngrediente;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingrediente that = (Ingrediente) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(traduccion, that.traduccion) && Objects.equals(PlatoIngrediente, that.PlatoIngrediente);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, traduccion, PlatoIngrediente);
+    }
+
+    @Override
+    public String toString() {
+        return "Ingrediente{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", traduccion='" + traduccion + '\'' +
+                ", PlatoIngrediente=" + PlatoIngrediente +
+                '}';
     }
 }
--- Dev/FrontEnd/src/pages/CrearIngrediente.vue
+++ Dev/FrontEnd/src/pages/CrearIngrediente.vue
@@ -9,7 +9,15 @@
         <q-input
           filled
           v-model="name"
-          label="Your name *"
+          label="Ingrediente"
+          hint="Name and surname"
+          lazy-rules
+          :rules="[ val => val && val.length > 0 || 'Please type something']"
+        />
+        <q-input
+          filled
+          v-model="traduccion"
+          label="Ingrediente en ingles"
           hint="Name and surname"
           lazy-rules
           :rules="[ val => val && val.length > 0 || 'Please type something']"
@@ -24,11 +32,12 @@
     </div>
     <div class="q-pa-md">
       <q-list bordered>
-        <q-item clickable v-ripple v-for="ingrediente in ing" :key="id">
+        <q-item clickable v-ripple v-for="ingrediente in ing" :key="ingrediente.id">
           <q-item-section thumbnail>
             <img src="https://cdn.quasar.dev/img/mountains.jpg">
           </q-item-section>
           <q-item-section>{{ ingrediente.name}}</q-item-section>
+          <q-item-section>{{ ingrediente.traduccion}}</q-item-section>
           <q-btn color="red">Borrar</q-btn>
         </q-item>
       </q-list>
@@ -46,17 +55,20 @@ export default {
     return {
       url_server_api: SETTINGS.URL_SERVER_API,
       name: null,
+      traduccion:null,
       ing: []
     }
   },
   created() {
     this.getIngredientes();
+
   },
 
   methods: {
     async onSubmit() {
       let add = await this.$axios.post(this.url_server_api + '/admin/addingredient', {
-        name: this.name
+        name: this.name,
+        traduccion: this.traduccion
       });
       if (add.request.status === 202) {
         this.$q.notify({
@@ -78,7 +90,7 @@ export default {
     },
     onReset() {
       this.name = null
-      this.age = null
+      this.traduccion = null
     },
     async getIngredientes() {
       let ingredientes = await this.$axios.get(this.url_server_api + '/admin/getAllIngredientes')
