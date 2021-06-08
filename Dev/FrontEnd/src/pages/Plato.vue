ba931cffb85bbc15ba08624ed00abc8a7d84e8b5
Skrimou <krimo.bensaid@gmail.com>
2021-06-08 15:02:39 +0200

    se ve una grafica en forma de donut, falta pasarle los datos

--- Dev/FrontEnd/src/pages/Plato.vue
+++ Dev/FrontEnd/src/pages/Plato.vue
@@ -1,20 +1,44 @@
 <template>
-
+  <div class="col-md-6 col-sm-12 col-xs-12">
+    <div id="chart">
+      <apexchart type="donut" :options="chartOptions" :series="series"></apexchart>
+    </div>
+  </div>
 </template>
 
 <script>
 import {SETTINGS} from "src/settings";
-
+import VueApexCharts from 'vue-apexcharts'
 export default {
   props: ['id'],
   name: "Plato.vue",
+  components: {
+    apexchart: VueApexCharts,
+  },
   data () {
     return {
       id: '',
-      url_server_api: SETTINGS.URL_SERVER_API
+      url_server_api: SETTINGS.URL_SERVER_API,
+      series: [44, 55, 41, 17, 15],
+      chartOptions: {
+        chart: {
+          type: 'donut',
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }]
+      },
     }
   },
-  created() {
+  async created() {
     this.id = this.$router.currentRoute.params.id
 
   },
