332559b2620670c85b6c8e727a3f127d3315fb95
Skrimou <krimo.bensaid@gmail.com>
2021-06-09 13:02:02 +0200

    La grafica en la vue del plato ya carga con los porcentajes y nombres de los valores nutricionales totales del plato

--- Dev/FrontEnd/src/pages/Plato.vue
+++ Dev/FrontEnd/src/pages/Plato.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-md-6 col-sm-12 col-xs-12">
     <div id="chart">
-      <apexchart type="donut" :options="chartOptions" :series="series"></apexchart>
+      <apexchart type="donut"  :options="chartOptions" :series="series"></apexchart>
     </div>
   </div>
 </template>
@@ -17,13 +17,28 @@ export default {
   },
   data () {
     return {
+      plato: '',
       id: '',
       url_server_api: SETTINGS.URL_SERVER_API,
-      series: [44, 55, 41, 17, 15],
+      series: [44, 55, 41, 17],
       chartOptions: {
         chart: {
           type: 'donut',
         },
+        plotOptions: {
+          pie: {
+            donut: {
+              labels: {
+                show: true,
+                total: {
+                  showAlways: true,
+                  show: true
+                }
+              }
+            }
+          }
+        },
+        labels: ["Azucar", "Energia", "Grasas", "Proteinas"],
         responsive: [{
           breakpoint: 480,
           options: {
@@ -40,6 +55,12 @@ export default {
   },
   async created() {
     this.id = this.$router.currentRoute.params.id
+    let platoFetch = await this.$axios.post(this.url_server_api + '/getPlatoById',{
+      idplato: this.id
+    });
+    this.plato = platoFetch.data;
+    this.series = [this.plato.azucar,this.plato.energia,this.plato.grasas,this.plato.proteinas]
+
 
   },
 
