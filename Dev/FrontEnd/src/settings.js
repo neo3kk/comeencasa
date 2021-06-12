044b81b26100666d66d6f6461d7ea6c33a770355
Jlopez <neo3kk@gmail.com>
2021-06-12 10:19:25 +0200

    se generan los iconos personalizados

--- Dev/FrontEnd/src/settings.js
+++ Dev/FrontEnd/src/settings.js
@@ -1,16 +1,16 @@
-/*
+
 export const SETTINGS = {
   URL_SERVER_API: "http://localhost:5000/",
   STRIPE_PUBLISHABLE_KEY: "pk_test_LESShQ47cPmtRV4MhkefvSax00lZTqQsOv"
 };
-*/
+
 
 //////////////// SETTINGS PARA PRODUCCIÓN ////////////
-
+/*
 export const SETTINGS = {
   URL_SERVER_API: "http://comeencasa.duckdns.org:5000/",
   STRIPE_PUBLISHABLE_KEY: "pk_test_LESShQ47cPmtRV4MhkefvSax00lZTqQsOv"
-};
+};*/
 
 
 /*
