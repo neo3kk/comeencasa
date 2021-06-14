90f0d71b0ac3647a35d70738a1d0f0e22e5c0060
Jlopez <neo3kk@gmail.com>
2021-06-14 07:51:09 +0200

    ya se ve el precio final del carrito en el boton de pagar

--- Dev/FrontEnd/src/settings.js
+++ Dev/FrontEnd/src/settings.js
@@ -1,9 +1,9 @@
+//////////////// SETTINGS PARA DESARROLLO ////////////
 
 export const SETTINGS = {
   URL_SERVER_API: "http://localhost:5000/",
   STRIPE_PUBLISHABLE_KEY: "pk_test_LESShQ47cPmtRV4MhkefvSax00lZTqQsOv"
 };
-
 
 //////////////// SETTINGS PARA PRODUCCIÓN ////////////
 /*
@@ -11,19 +11,6 @@ export const SETTINGS = {
   URL_SERVER_API: "http://comeencasa.duckdns.org:5000/",
   STRIPE_PUBLISHABLE_KEY: "pk_test_LESShQ47cPmtRV4MhkefvSax00lZTqQsOv"
 };*/
-
-
-/*
-
-export const SETTINGS = {
-  URL_SERVER_OAUTH: "http://node.theavengersblog.com",
-  URL_SERVER_API: "http://tomcat.theavengersblog.com"
-};
-
-*/
-
-
-
 
 
 
--- Dev/startup.sh
+++ Dev/startup.sh
@@ -2,6 +2,6 @@
 
 cd ./FrontEnd
 docker-compose run node npm install
-docker-compose up -d
+docker-compose up -d node
 cd ../BackEnd
 docker-compose up -d
